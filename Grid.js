a8ff100f264fc53f9e4685d857c003cb1baf76c1
Älex <alexjoshua@gmail.com>
2022-05-09 07:33:56 +0200

    set vars in js

--- Grid.js
+++ Grid.js
@@ -1,3 +1,13 @@
+const gridSize = 4;
+const cellSize = 20;
+const cellGap = 2;
+const cellRadius = 1;
+
 export default class Grid {
-  constructor(gridElement)
+  constructor(gridElement) {
+    gridElement.style.setProperty('--grid-size', `${gridSize}`);
+    gridElement.style.setProperty('--cell-size', `${cellSize}vmin`);
+    gridElement.style.setProperty('--cell-gap', `${cellGap}vmin`);
+    gridElement.style.setProperty('--cell-radius', `${cellRadius}vmin`);
+  }
 }
---
+++ script.js
@@ -0,0 +1,5 @@
+import Grid from './Grid.js';
+
+console.log('SCRIPT IS LOADED');
+const gameBoard = document.getElementById('game-board');
+const grid = new Grid(gameBoard);
