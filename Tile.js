c6547cd33bfafcbdbd5e0d3b6f319d748de5603f
Alex Joshua <ialexjoshua@gmail.com>
2022-05-11 12:31:30 +0200

    set up tiles with colors

---
+++ Tile.js
@@ -0,0 +1,32 @@
+export default class Tile{
+  #tileElement
+  #value
+  #x
+  #y
+  constructor(tileContainer, value = Math.random() > .5 ? 2 : 4){
+    this.#tileElement = document.createElement('div');
+    this.#tileElement.classList.add("tile")
+    tileContainer.append(this.#tileElement)
+    this.value = value
+
+  }
+
+  set value(tileValue){
+    this.#value = tileValue
+    this.#tileElement.textContent = tileValue
+    const power = Math.log2(tileValue)
+    const backgroundBrightness = 100 - power * 9
+    this.#tileElement.style.setProperty('--tile-brightness', `${backgroundBrightness}%`)
+    this.#tileElement.style.setProperty('--text-brightness', `${backgroundBrightness <= 50 ?90 : 10}%`)
+  }
+
+  set x(value){
+    this.#x = value
+    this.#tileElement.style.setProperty("--x-tile", value)
+  }
+
+  set y(value){
+    this.#y = value
+    this.#tileElement.style.setProperty("--y-tile", value)
+  }
+}
